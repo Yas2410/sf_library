2d71be2d583c7b492882ec790b53e3664da1c718
Yas2410 <yasemin.kocak@lapiscine.pro>
2020-03-24 11:59:19 +0100

    Update 2 (with get/url)

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,16 +24,60 @@ class BookController extends AbstractController
     }
 
     /**
-     * @route("/book/{id}", name="book")
+     * @route("/book/show/{id}", name="book")
      */
     public function book(BookRepository $bookRepository, $id)
     {
-        $books = $bookRepository->find($id);
+        $book = $bookRepository->find($id);
 
         return $this->render('book.html.twig', [
-            'books' => $books
+            'book' => $book
         ]);
     }
+
+    /**
+     * @route("/book/insert", name="book_insert")
+     * @param EntityManagerInterface $entityManager
+     * @param Request $request
+     * @return Response
+     */
+    /*public function insertBook (EntityManagerInterface $entityManager, Request $request)
+    {
+        $book = new Book();
+
+        $book->setTitle("L'amie Prodigieuse");
+        $book->setAuthor("Elena FERRANTE");
+        $book->setNbPages("440");
+        $book->setResume("Test insertion nouveau livre");
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return new Response( "test");
+
+    }*/
+
+    public function insertBook (EntityManagerInterface $entityManager, Request $request)
+    {
+        $book = new Book();
+
+        //Récupérer les éléments à partir de l'URL
+
+        $title = $request->query->get("title");
+        $author = $request->query->get("author");
+        $nbPages = $request->query->get("nbPages");
+        $resume = $request->query->get("resume");
+
+        $book->setTitle($title);
+        $book->setAuthor($author);
+        $book->setNbPages($nbPages);
+        $book->setResume($resume);
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+        return new Response( "test2");
+    }
+
 
 }
 
