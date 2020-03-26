be646835c6a0a6953b8ffdabea06d1f7d40bb187
Yas2410 <yasemin.kocak@lapiscine.pro>
2020-03-26 16:49:35 +0100

    Front Files

---
+++ src/Controller/Front/AuthorController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    /**
+     * @Route("front/authors", name="front_authors")
+     */
+    public function authors (AuthorRepository $authorRepository)
+    {
+        $authors = $authorRepository->findAll();
+        return $this->render('front/author/authors.html.twig', [
+            'authors' => $authors
+        ]);
+    }
+
+    /**
+     * @route("front/author/show/{id}", name="front_author")
+     */
+    public function author(AuthorRepository $authorRepository, $id)
+    {
+        $author = $authorRepository->find($id);
+
+        return $this->render('front/author/author.html.twig', [
+            'author' => $author
+        ]);
+    }
+
+    /**
+     * @route("front/author/search", name="front_author_search")
+     */
+    public function searchByBiography(AuthorRepository $authorRepository, Request $request)
+    {
+        $search = $request->query->get('search');
+        $authors = $authorRepository->getByWordInBiography($search);
+
+        return $this->render('front/author/searchBio.html.twig', [
+            'search'=>$search, 'authors'=>$authors
+        ]);
+    }
+
+
+
+}
---
+++ src/Controller/Front/BookController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookController extends AbstractController
+{
+    /**
+     * @Route("front/books", name="front_books")
+     */
+    public function books (BookRepository $bookRepository)
+    {
+        $books = $bookRepository->findAll();
+        return $this->render('front/book/books.html.twig', [
+            'books' => $books
+            ]);
+    }
+
+    /**
+     * @route("front/book/show/{id}", name="front_book")
+     */
+    public function book(BookRepository $bookRepository, $id)
+    {
+        $book = $bookRepository->find($id);
+
+        return $this->render('front/book/book.html.twig', [
+            'book' => $book
+        ]);
+    }
+
+    /**
+     * @route("front/book/search", name="front_book_search")
+     */
+    public function searchByResume(BookRepository $bookRepository, Request $request)
+    {
+        $search = $request->query->get('search');
+        $books = $bookRepository->getByWordInResume($search);
+
+        return $this->render('front/book/search.html.twig', [
+            'search'=>$search, 'books'=>$books
+        ]);
+    }
+
+}
+
